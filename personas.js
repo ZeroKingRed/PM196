f2925dae97cde5da9dafe34577c7c42d34958143
ZeroKingRed <140178107+ZeroKingRed@users.noreply.github.com>
2025-05-20 12:45:56 -0600

    Codigo de filtrado de luis

--- personas.js
+++ personas.js
@@ -2,7 +2,7 @@ const personas = [
   { nombre: "Ana", edad: 22 },
   { nombre: "Luis", edad: 35 },
   { nombre: "María", edad: 28 },
-  { nombre: "Luis", edad: 18 },
+  { nombre: "Luis", edad: 17 },
 
 ];
 
